f5b435e8c4162348dc7afc9a7dc9ad4732936d67
Francisco Barroso <franciscobarroso@grupofortes.com.br>
2016-09-23 17:07:05 -0300

    test Unmount

--- src/AppButtonCount.js
+++ src/AppButtonCount.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class AppButtonCount extends React.Component {
   constructor() {
@@ -8,14 +9,49 @@ class AppButtonCount extends React.Component {
   }
 
   update() {
-    this.setState({ val: this.state.val + 1 })
+    this.setState({ val: this.state.val + 1 });
+  }
+
+  componentWillMount(){
+    console.log('mounting...');
+  }
+
+  componentDidMount(){
+    console.log('mounted...');
+  }
+
+  componentWillUnmount(){
+    console.log('bye!...');
   }
 
   render() {
-    console.log('rendeting...')
+    console.log('rendeting...');
     return <button className="btn btn-default"
       onClick={this.update}>{this.state.val}</button>
   }
 }
 
-export default AppButtonCount
+class Wrapper extends React.Component {
+  constructor(){
+    super();
+  }
+
+  mount(){
+    ReactDOM.render(<AppButtonCount />, document.getElementById('a'));
+  }
+  unmount(){
+    ReactDOM.unmountComponentAtNode(document.getElementById('a'));
+  }
+
+  render(){
+    return (
+      <div>
+        <button onClick={this.mount.bind(this)}>Mount</button>
+        <button onClick={this.unmount.bind(this)}>Unmount</button>
+        <div id="a"></div>
+      </div>
+    )
+  }
+}
+
+export default Wrapper
